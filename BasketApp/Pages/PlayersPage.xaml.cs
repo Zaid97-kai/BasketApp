8f0ffc0fb8d27120ff04392ea1bda4ee63ef4ebc
zzmingaliev <zzmingaliev@gmail.com>
2021-07-04 15:50:47 +0300

    Реализовано заполнение DataGrid ver.1

--- BasketApp/Pages/PlayersPage.xaml.cs
+++ BasketApp/Pages/PlayersPage.xaml.cs
@@ -18,6 +18,18 @@ namespace BasketApp.Pages
     /// <summary>
     /// Логика взаимодействия для PlayersPage.xaml
     /// </summary>
+    public class TempPlayer
+    {
+        public byte[] Img { get; set; }
+        public int PlayerId { get; set; }
+        public string Name { get; set; }
+        public string CountryCode { get; set; }
+        public string Position { get; set; }
+        public decimal Weight { get; set; }
+        public decimal Height { get; set; }
+        public string Country { get; set; }
+        public System.DateTime JoinYear { get; set; }
+    }
     public partial class PlayersPage : Page
     {
         public PlayersPage()
@@ -25,7 +37,24 @@ namespace BasketApp.Pages
             InitializeComponent();
 
             NBAShort_08Entities context = new NBAShort_08Entities();
-            GridPlayers.ItemsSource = context.Player.ToList();
+            //GridPlayers.ItemsSource = context.Player.ToList();
+            List<TempPlayer> tempPlayers = new List<TempPlayer>();
+            foreach (Player item in context.Player)
+            {
+                tempPlayers.Add(new TempPlayer
+                {
+                    Img = item.Img,
+                    PlayerId = item.PlayerId,
+                    Name = item.Name,
+                    Position = item.Position.Name,
+                    Weight = item.Weight,
+                    Height = item.Height,
+                    JoinYear = item.JoinYear,
+                    Country = item.Country.CountryName,
+                    CountryCode = item.CountryCode
+                });
+            }
+            GridPlayers.ItemsSource = tempPlayers;
         }
     }
 }
